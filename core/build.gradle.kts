5d111888124baa8cb003eb04592fbf92c38504ad
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-09-25 12:02:34 +0200

    Fix configuration

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.maven.publish)
 
-    // Apply the java-library plugin for API and implementation separation.
     `java-library`
     signing
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
@@ -19,25 +16,26 @@ dependencies {
 
 val gitTagProvider = providers.exec {
     commandLine("git", "describe", "--tags", "--abbrev=0")
+    workingDir = rootProject.projectDir
+    isIgnoreExitValue = true
 }.standardOutput.asText.map { it.trim() }.orNull
 
 val gitHashProvider = providers.exec {
     commandLine("git", "rev-parse", "--short", "HEAD")
+    workingDir = rootProject.projectDir
+    isIgnoreExitValue = true
 }.standardOutput.asText.map { it.trim() }.orNull
 
 version = gitTagProvider ?: gitHashProvider ?: "dev"
 
 testing {
     suites {
-        // Configure the built-in test suite
         val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
             useKotlinTest("2.2.0")
         }
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
