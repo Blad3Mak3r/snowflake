58ad3fd40bf8cc16c299d0cd0fbe9da85ab7ca29
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-09-27 09:22:55 +0200

    build: Rename SnowflakeGenerator to SnowflakeFactory, adapt test, and new data class Snowflake

--- exposed-dao/src/main/kotlin/io/github/blad3mak3r/snowflake/exposed/dao/SnowflakeIdTable.kt
+++ exposed-dao/src/main/kotlin/io/github/blad3mak3r/snowflake/exposed/dao/SnowflakeIdTable.kt
@@ -1,6 +1,6 @@
 package io.github.blad3mak3r.snowflake.exposed.dao
 
-import io.github.blad3mak3r.snowflake.core.SnowflakeGenerator
+import io.github.blad3mak3r.snowflake.core.SnowflakeFactory
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.v1.core.Column
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
@@ -8,11 +8,11 @@ import org.jetbrains.exposed.v1.core.dao.id.IdTable
 
 open class SnowflakeIdTable(
     name: String,
-    private val generator: SnowflakeGenerator
+    private val factory: SnowflakeFactory
 ) : IdTable<Long>(name) {
 
     final override val id: Column<EntityID<Long>> = long("id")
-        .clientDefault { runBlocking { generator.nextId() } }
+        .clientDefault { runBlocking { factory.nextId() } }
         .uniqueIndex()
         .entityId()
 
--- exposed/src/main/kotlin/io/github/blad3mak3r/snowflake/exposed/core/Exposed.kt
+++ exposed/src/main/kotlin/io/github/blad3mak3r/snowflake/exposed/core/Exposed.kt
@@ -1,14 +1,14 @@
 package io.github.blad3mak3r.snowflake.exposed.core
 
-import io.github.blad3mak3r.snowflake.core.SnowflakeGenerator
+import io.github.blad3mak3r.snowflake.core.SnowflakeFactory
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.v1.core.Column
 import org.jetbrains.exposed.v1.core.ResultRow
 import org.jetbrains.exposed.v1.core.Table
 
 @Suppress("unused")
-fun Table.snowflake(name: String, generator: SnowflakeGenerator): Column<Long> {
-    return long(name).clientDefault { runBlocking { generator.nextId() } }
+fun Table.snowflake(name: String, factory: SnowflakeFactory): Column<Long> {
+    return long(name).clientDefault { runBlocking { factory.nextId() } }
 }
 
 @Suppress("unused")
